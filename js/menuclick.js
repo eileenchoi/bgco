f2670d75db885f14e847ef3025e2ceb15e5234f9
eileenchoi <eileen.h.choi@gmail.com>
2019-12-19 21:35:04 +0000

    jquery

--- js/menuclick.js
+++ js/menuclick.js
@@ -1,13 +1,31 @@
-import { brotliDecompressSync } from "zlib";
 
 
 
-  $(document).ready(function(){
+  // jQuery(document).ready(function(){
 
-    jQuery(".menu li a").click(function () {
-        jQuery(".nav-primary").toggle();
-        jQuery(".menu-toggle").removeClass('activated');
-    });
+  //   jQuery('.menu li a').click(function () {
+  //       jQuery(".nav-primary").toggle();
+  //       jQuery(".menu-toggle").removeClass('activated');
+  //       console.log("wtf");
+  //   });
+  
+  // });
+
+  jQuery(document).ready(function(){
+
+    if (jQuery(".menu-toggle").hasClass("activated") && jQuery(".site-container").live('click', function() {
+      
+      jQuery(".menu-toggle").removeClass('activated');
+
+    } else {
+
+      jQuery('.menu li a').click(function () {
+              jQuery(".nav-primary").toggle();
+              jQuery(".menu-toggle").removeClass('activated');
+              console.log("wtf");
+          })
+    }
+
   
   });
 
