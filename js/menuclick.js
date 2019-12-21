9e6aa089c6838f09684effccc6c17af047e57ecb
eileenchoi <eileen.h.choi@gmail.com>
2019-12-21 21:58:15 +0000

    jquery

--- js/menuclick.js
+++ js/menuclick.js
@@ -3,30 +3,59 @@
 
   // jQuery(document).ready(function(){
 
-  //   jQuery('.menu li a').click(function () {
+  //  jQuery('.menu li a').click(function () {
   //       jQuery(".nav-primary").toggle();
   //       jQuery(".menu-toggle").removeClass('activated');
   //       console.log("wtf");
   //   });
+
+  //   if (jQuery('.nav-primary').is(':visible')) {
+
+  //   jQuery('html').click(function () {
+  //     jQuery(".nav-primary").toggle();
+  //     jQuery(".menu-toggle").removeClass('activated');
+  //     console.log("IT WORKED");
+  // });
+
+  //   }
+
   
   // });
 
-  jQuery(document).ready(function(){
-
-    if (jQuery(".menu-toggle").hasClass("activated") && jQuery(".site-container").live('click', function() {
-      
-      jQuery(".menu-toggle").removeClass('activated');
-
-    } else {
-
-      jQuery('.menu li a').click(function () {
-              jQuery(".nav-primary").toggle();
-              jQuery(".menu-toggle").removeClass('activated');
-              console.log("wtf");
-          })
-    }
 
   
-  });
 
+  // jQuery(document).ready(function(){
+
+  //   if (jQuery(".menu-toggle").hasClass("activated") && $(".site-container").on('click')) {
+  //     jQuery(".menu-toggle").removeClass("activated");
+
+  //   } else {
+
+  //     jQuery(".nav-primary").toggle();
+  //     jQuery(".menu-toggle").removeClass("activated");
+  //     console.log("HUZZAH!!!!!!!!!!");
+
+  //   }
+
+
+  // });
+  
+
+  // jQuery(document).ready(function(){
+
+  //   if (jQuery(".menu-toggle").hasClass("activated")) {
+  //     jQuery(".nav-primary").toggle();
+  //     jQuery(".menu-toggle").removeClass("activated");
+
+  //   } else if (jQuery(".site-container").live('click')) {
+  //     jQuery(".menu-toggle").removeClass("activated");
+  //   } else{
+
+  //     console.log("did it work");
+
+  //   }
+
+  // });
+  
   
