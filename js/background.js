2dfd7cd3cb93b5f5dd9b854f4691c3c425924c8f
eileenchoi <eileen.h.choi@gmail.com>
2020-01-28 17:26:42 +0000

    final review complete

--- js/background.js
+++ js/background.js
@@ -4,16 +4,14 @@
 function randomImage(){
     var images = [
         '/wp-content/uploads/2019/12/fourcourts-full-scaled.jpeg',
-        '/wp-content/uploads/2019/12/longroom-full-scaled.jpeg',
-        '/wp-content/uploads/2019/12/dublincity-full-scaled.jpeg',
+        // '/wp-content/uploads/2019/12/longroom-full-scaled.jpeg',
+        // '/wp-content/uploads/2019/12/dublincity-full-scaled.jpeg',
         '/wp-content/uploads/2019/12/trinitycollege-full-scaled.jpg'
-
        
         
     ];
     var size = images.length;
     var x = Math.floor(size * Math.random());
-    // console.log(x);
     var element = document.getElementsByClassName('front-page-1');
 
     element[0].style["background-image"] = "url("+ images[x] + ")";
